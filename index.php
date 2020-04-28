3ab55d37e43540e78c658ec7b67b90a92e8c92b9
Brandon Stenhouse <bstenhouse27@gmail.com>
2020-04-28 19:50:51 +1000

    Init Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        
+        <?php include_once './src/php/head.php' ?>
+        
+        <script>
+
+        </script>
+
+        <style>
+
+        </style>
+
+    </head>
+
+    <body>
+
+        <?php include_once '/src/php/header.php' ?>
+        <?php include_once '/src/php/navi.php' ?>
+
+        <main>
+
+        </main>
+
+        <?php include_once '/src/php/footer.php' ?>
+        
+    </body>
+
+</html>
---
+++ src/php/header.php
@@ -0,0 +1,5 @@
+<header>
+    <a href="/">
+        <img src="https://images.squarespace-cdn.com/content/v1/5c86e337809d8e6661fefe74/1553678794438-RKS01IFML86CWNAL5H0U/ke17ZwdGBToddI8pDm48kJvl0B5JyNnZQEsxwmUGUyJZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpzpwZTMBhVSD7_TjML_Vm4tnny7Is4pP75A23jH_AA6liS9B7i30XbDYQDoKDTURzM/what-is-lethean-text.png?format=1000w">
+    </a>
+</header>
