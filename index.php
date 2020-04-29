29518acb9343fdf7cad30019eae40b739d50eb8f
Brandon Stenhouse <bstenhouse27@gmail.com>
2020-04-29 22:02:51 +1000

    New Layout

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <head>
         
         <?php include_once './src/php/head.php' ?>
+
+        <title>Lethean VPN</title>
         
         <script>
 
@@ -26,38 +28,41 @@
         <?php include_once './src/php/navi.php' ?>
 
         <main class="container">
-            <?php foreach ($json->providers as $i => $provider) { 
-                if ($provider->type == "vpn") {?>
-            <div class="title row">
-                <h3><?php echo $provider->providerName ?></h3>
-            </div>
-            <div class="provider row">
-                <div class="providerDetails col">
-                    <p>Description:<br/><span class="indent"><?php echo $provider->name ?></span></p>
-                    <p>Payment ID:<br/><span class="indent"><?php echo $provider->id ?></span></p>
-                    <p>Provider Wallet:<br/><span class="indent"><?php echo $provider->providerWallet ?></span></p>
+            <?php 
+                foreach ($json->providers as $i => $provider) { 
+                    if ($provider->type == "vpn") {
+            ?>
+
+            <div class="card">
+                <img src="" alt="" />
+                <div class="card-header">
+                    <h5 class-="card-title"><?php echo $provider->providerName; ?></h5> 
                 </div>
-                <div class="vpnDetails col">
-                    <p>Download Speed:<br/><span class="indent"><?php echo $provider->downloadSpeed ?></span></p>
-                    <p>Upload Speed:<br/><span class="indent"><?php echo $provider->uploadSpeed ?></span></p>
-                    <p>Speed:<br/><span class="indent"><?php echo $provider->mSpeed ?></span></p>
-                    <p>Stability:<br/><span class="indent"><?php echo $provider->mStability ?></span></p>
+                <div class="card-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <p>Description: <?php echo $provider->name; ?></p>
+                                <p>Stability: <?php echo $provider->mStability; ?></p>
+                            </div>
+                            <div class="col">
+                                <p>Download Speed: <?php echo $provider->downloadSpeed/(1024*1024); ?>Mbps</p>
+                                <p>Upload Speed: <?php echo $provider->uploadSpeed/(1024*1024); ?>Mbps</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="price col">
-                    <p>Cost:<br/><span class="indent"><?php echo $provider->cost ?></span></p>
+                <div class="card-footer">
+                    <?php foreach ($provider->vpn as $j => $vpn) { ?>
+                    <button class="btn btn-outline-primary download">Endpoint <?php echo ++$j; ?></button>
+                    <?php } ?>
                 </div>
             </div>
-            <div class="download row text-right">
-                <?php foreach ($provider->vpn as $j => $vpn) { ?>
-                <button class="btn btn-outline-primary download">Endpoint <?php echo ++$j; ?>
-                <?php } ?>
-            </div>
-            <div class="row">
-            <br/>
-            </div>
+            
             <?php }} ?>
-            </div>
+
         </main>
+
         <?php include_once './src/php/footer.php' ?>
 
     </body>
