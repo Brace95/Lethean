465705c61963587198938db57d99f6e2a4a3fd01
Brandon Stenhouse <bstenhouse27@gmail.com>
2020-04-29 22:24:24 +1000

    Layout adjustment

--- index.php
+++ index.php
@@ -46,17 +46,15 @@
                                 <p>Stability: <?php echo $provider->mStability; ?></p>
                             </div>
                             <div class="col">
-                                <p>Download Speed: <?php echo $provider->downloadSpeed/(1000*1000); ?>Mbps</p>
-                                <p>Upload Speed: <?php echo $provider->uploadSpeed/(1000*1000); ?>Mbps</p>
+                                <p>Download Speed: <?php echo $provider->downloadSpeed/(1000*1000); ?> Mbps</p>
+                                <p>Upload Speed: <?php echo $provider->uploadSpeed/(1000*1000); ?> Mbps</p>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col align-self-end">
-                                <p class="">
-                                    <?php echo $provider->cost; ?>
-                                    <img class="bullet" src="https://images.squarespace-cdn.com/content/v1/5c86e337809d8e6661fefe74/1554742652993-GORMP78JS81V3NRRO0CQ/ke17ZwdGBToddI8pDm48kJycfsYb1urLU93EpFqOTQmoCXeSvxnTEQmG4uwOsdIceAoHiyRoc52GMN5_2H8WpwgL4wI2D_AHIh_vss1JoxZD3UMNL-MOqG5-2RCLT_RsnJVHPJJ1-XiWt_skTFO5QQ/favicon.ico?format=">
-                                    /m
-                                </p>
+                                <?php echo $provider->cost; ?>
+                                <img class="bullet" src="https://images.squarespace-cdn.com/content/v1/5c86e337809d8e6661fefe74/1554742652993-GORMP78JS81V3NRRO0CQ/ke17ZwdGBToddI8pDm48kJycfsYb1urLU93EpFqOTQmoCXeSvxnTEQmG4uwOsdIceAoHiyRoc52GMN5_2H8WpwgL4wI2D_AHIh_vss1JoxZD3UMNL-MOqG5-2RCLT_RsnJVHPJJ1-XiWt_skTFO5QQ/favicon.ico?format=">
+                                /m
                             </div>
                         </div>
                     </div>
