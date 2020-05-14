3b564ba1b18378d2a8ffe64960f98c456fb76417
bodane <slobodan_susa@hotmail.com>
2020-05-14 23:55:38 +1000

    Text alignment improvements with styling

--- index.php
+++ index.php
@@ -79,83 +79,85 @@
                 <div class="card-body">
                     <div class="container">
                         <div class="row">
-                            <div class="col text-center">
-                                <?php echo "Provider ID"; ?>
+                            <div class="col text-right">
+                                <p>Provider ID</p>
                             </div>
-                            <div class="col text-center">
-                                <?php echo "Stability"; ?>
+                            <div class="col-6 text-left">
+                                <span class="card-value"><?php echo $provider->id; ?></span>
                             </div>
-                            <div class="col text-center">
-                                <?php 
+                        </div>
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col text-right">
+                                Stability
+                            </div>
+                            <div class="col-6 text-left">
+                                <span class="card-value"><?php 
                                 if ($provider->mStability == "") {
                                     echo "Not Rated";
                                 }
                                 else {
                                     echo $provider->mStability;
                                 };
-                                ?>
+                                ?></span>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col text-center">
-                                <?php echo $provider->id; ?>
+                            <div class="col"></div>
+                            <div class="col text-right">
+                                Speed
                             </div>
-                            <div class="col text-center">
-                                <?php echo "Speed"; ?>
-                            </div>
-                            <div class="col text-center">
-                                <?php 
+                            <div class="col-6 text-left">
+                                <span class="card-value"><?php 
                                 if ($provider->mSpeed == "") {
                                     echo "Not Rated";
                                 }
                                 else {
                                 echo $provider->mSpeed; 
                                 };
-                                ?>
+                                ?></span>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col text-center">
+                            <div class="col">
                             </div>
-                            <div class="col text-center">
-                                <?php echo "Download"; ?>
+                            <div class="col text-right">
+                                Download
                             </div>
-                            <div class="col text-center">
-                                <?php echo number_format($provider->downloadSpeed/(1000*1000), 2); ?> Mbps
+                            <div class="col-6 text-left">
+                                <span class="card-value"><?php echo number_format($provider->downloadSpeed/(1000*1000), 2); ?> Mbps</span>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col text-center">
+                            <div class="col">
                             </div>
-                            <div class="col text-center">
-                                <?php echo "Upload"; ?>
+                            <div class="col text-right">
+                                Upload
                             </div>
-                            <div class="col text-center">
-                                <?php echo number_format($provider->uploadSpeed/(1000*1000), 2); ?> Mbps
+                            <div class="col-6 text-left">
+                                <span class="card-value"><?php echo number_format($provider->uploadSpeed/(1000*1000), 2); ?> Mbps</span>
                             </div>
                         </div> 
                         <div class="row">
-                            <div class="col text-center">
+                            <div class="col">
                             </div>
-                            <div class="col text-center">
-                                <?php echo "Cost"; ?>
+                            <div class="col text-right">
+                                Cost
                             </div>                            
-                            <div class="col text-center">
+                            <div class="col-6 text-left">
                                 <img class="bullet" src="<?php echo $lethean_logo; ?>" >
-                                <?php echo number_format($provider->cost, 2); ?>
-                                /m
+                                <span class="card-value"><?php echo number_format($provider->cost, 2); ?>
+                                /m</span>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col text-center">
+                            <div class="col text-right">
+                                Min Cost
                             </div>
-                            <div class="col text-center">
-                                <?php echo "Min cost "; ?>
-                            </div>
-                            <div class="col text-center">
-                                <em><?php echo ($provider->firstPrePaidMinutes * $provider->cost); ?></em>
+                            <div class="col-6 text-left">
+                                <span class="card-value"><em><?php echo ($provider->firstPrePaidMinutes * $provider->cost); ?></em>
                                 <img class="bullet" src="<?php echo $lethean_logo; ?>" >
-                                <em><?php echo " / " . $provider->firstPrePaidMinutes . " mins"; ?></em>
+                                <em><?php echo " / " . $provider->firstPrePaidMinutes . " mins"; ?></em></span>
                             </div>
                         </div>
                     </div>
@@ -167,16 +169,16 @@
                         <div class="card card-body">
                             <div class="container">
                                 <div class="row">
-                                    <p class="col">Endpoint: <?php echo $vpn->endpoint; ?></p>
+                                    <p class="col">Endpoint <span class="card-value"><?php echo $vpn->endpoint; ?><span></p>
                                 </div>
                                 <div class="row">
-                                    <p class="col">Port: <?php echo $vpn->port; ?></p>
+                                    <p class="col">Port <span class="card-value"><?php echo $vpn->port; ?></span></p>
                                 </div>
                                 <div class="row">
-                                    <p class="col">Endpoint Wallet: <?php echo $provider->providerWallet; ?></p>
+                                    <p class="col">Endpoint Wallet <span class="card-value"><?php echo $provider->providerWallet; ?></span></p>
                                 </div>
                                 <div class="row">
-                                    <p class="col">Payment ID (Required): <?php echo $provider->id; ?></p>
+                                    <p class="col">Payment ID (Required) <span class="card-value"><?php echo $provider->id; ?></span></p>
                                 </div>
                                 <div class="row">
                                     <input type="hidden" class="provider" value="<?php echo str_replace(" ", "_", $provider->providerName); ?>" />
